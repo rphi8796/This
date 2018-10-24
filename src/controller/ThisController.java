95ef48e507685e4f47d349b3c4625406287286b9
rphi8796 <jlkfop@gmail.com>
2018-10-24 13:16:18 -0600

    This is the code bat code

--- src/controller/ThisController.java
+++ src/controller/ThisController.java
@@ -1,28 +1,77 @@
 package controller;
 
-import java.util.ArrayList;
-
-import javax.swing.JOptionPane;
-
-
 public class ThisController
 {
 	public void start()
 	{
-		withoutx2();
+		withoutx2("xenophobia");
 	}
 	
 	public String withoutx2 (String str)
 	{
 		String output = "";
-		String fir = str.substring(0,1);
-		String sec = str.substring(1,2);
 		
-		if (fir.equalsIgnoreCase("x"))
+		
+		if (str.length() >= 2)
 		{
-			output = str.substring(1);
+			String fir = str.substring(0,1);
+			String sec = str.substring(1,2);
+			
+			if (fir.equalsIgnoreCase("x"))
+			{
+				output = str.substring(1);
+				
+				if (sec.equalsIgnoreCase("x"))
+				{
+					output = str.substring(2);
+				}
+				
+				else
+				{
+					output = str.substring(1);
+				}
+			}
+			
+			else if (!(fir.equalsIgnoreCase("x")))
+			{
+				if (sec.equalsIgnoreCase("x"))
+				{
+					output = str.substring(0, 1);
+					output += str.substring(2);
+				}
+				
+				else
+				{
+					output = str;
+				}
+				
+			}
+			
+			else 
+			{
+				output = str;
+			}
+
 		}
 		
+		else if (str.length() == 1)
+		{
+			if (str.equalsIgnoreCase("x"))
+			{
+				str = "";
+				output = str;
+			}
+			else
+			{
+				output = str;
+			}
+		}
+		
+		else
+		{
+			output = str;
+		}
 		return output;
 	}
 }
+	
