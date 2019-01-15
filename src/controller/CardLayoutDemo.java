455e9bf09f75ec5f604ffd7cae686eded354f839
rphi8796 <jlkfop@gmail.com>
2019-01-15 14:19:18 -0700

    I figured out how to do it with buttons

---
+++ src/controller/CardLayoutDemo.java
@@ -0,0 +1,93 @@
+package controller;
+
+/*
+ * CardLayoutDemo.java
+ *
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+ 
+public class CardLayoutDemo implements ItemListener 
+{
+    JPanel cards; //a panel that uses CardLayout
+    final static String BUTTONPANEL = "Card with JButtons";
+    final static String TEXTPANEL = "Card with JTextField";
+     
+    public void addComponentToPane(Container pane) 
+    {
+        //Put the JComboBox in a JPanel to get a nicer look.
+        JPanel comboBoxPane = new JPanel(); //use FlowLayout
+        String comboBoxItems[] = { BUTTONPANEL, TEXTPANEL };
+        JComboBox cb = new JComboBox(comboBoxItems);
+        JButton firstButton = new JButton("first");
+        JButton secondButton = new JButton("second");
+        firstButton.addItemListener(this);
+        secondButton.addItemListener(this);
+        cb.setEditable(false);
+        cb.addItemListener(this);
+        comboBoxPane.add(cb);
+        comboBoxPane.add(firstButton);
+        comboBoxPane.add(secondButton);
+         
+        //Create the "cards".
+        JPanel card1 = new JPanel();
+        card1.add(new JButton("Button 1"));
+        card1.add(new JButton("Button 2"));
+        card1.add(new JButton("Button 3"));
+         
+        JPanel card2 = new JPanel();
+        card2.add(new JTextField("TextField", 20));
+         
+        //Create the panel that contains the "cards".
+        cards = new JPanel(new CardLayout());
+        cards.add(card1, BUTTONPANEL);
+        cards.add(card2, TEXTPANEL);
+         
+        pane.add(comboBoxPane, BorderLayout.PAGE_START);
+        pane.add(cards, BorderLayout.CENTER);
+        
+        firstButton.addActionListener(new ActionListener()
+        {
+        	public void actionPerformed(ActionEvent mouseClick)
+        	{
+        		CardLayout cl = (CardLayout) (cards.getLayout());
+        		cl.show(cards, BUTTONPANEL);
+        	}
+        });
+        
+        secondButton.addActionListener(new ActionListener()
+        {
+        	public void actionPerformed(ActionEvent mouseClick)
+        	{
+        		CardLayout cl = (CardLayout) (cards.getLayout());
+        		cl.show(cards, TEXTPANEL);
+        	}
+        });
+    }
+    
+    public void itemStateChanged(ItemEvent evt) 
+    {
+        CardLayout cl = (CardLayout)(cards.getLayout());
+        cl.show(cards, (String)evt.getItem());
+    }
+     
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event dispatch thread.
+     */
+    public static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("CardLayoutDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         
+        //Create and set up the content pane.
+        CardLayoutDemo demo = new CardLayoutDemo();
+        demo.addComponentToPane(frame.getContentPane());
+         
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
---
+++ src/controller/Runner.java
@@ -0,0 +1,33 @@
+package controller;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class Runner
+{
+	public static void main(String[] args) {
+        /* Use an appropriate Look and Feel */
+        try {
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        /* Turn off metal's use of bold fonts */
+        UIManager.put("swing.boldMetal", Boolean.FALSE);
+         
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                CardLayoutDemo.createAndShowGUI();
+            }
+        });
+	}
+}
